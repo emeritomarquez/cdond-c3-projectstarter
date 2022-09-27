0d8b649a68ccd3ea28c2d1cfa19f30d4d95e4161
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-09-27 22:44:53 +0000

    Change external IP of Windows and Ubuntu22.0.4

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "52.25.131.75\n":
+    elif line == "35.89.22.225\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "35.155.247.29\n":
+    elif line == "35.89.199.166\n":
         pass
     else:
         rows.append(line)
