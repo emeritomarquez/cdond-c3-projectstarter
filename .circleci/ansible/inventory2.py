e2c307851722e5294fd4c0e196fc3147be249ceb
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-09-28 22:52:16 +0000

    Restart with new external IP

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "35.89.22.225\n":
+    elif line == "52.26.141.61\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "35.89.199.166\n":
+    elif line == "34.212.37.68\n":
         pass
     else:
         rows.append(line)
