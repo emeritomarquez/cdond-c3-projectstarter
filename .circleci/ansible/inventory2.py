bfad1bdad9f51ec40e3b89249687e97031357ff5
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-10-03 11:17:29 +0000

    Update EC2 external IP of workstations

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "35.91.125.73\n":
+    elif line == "34.222.58.23\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "34.211.146.75\n":
+    elif line == "35.89.171.40\n":
         pass
 #Cloud9 Public
     elif line == "18.237.236.204\n":
