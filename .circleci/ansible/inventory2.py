c8fab49262df608a428b40f09f8eb837b106df47
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-11-15 20:37:53 +0000

    Deploy frontend and list dist

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "34.219.144.182\n":
+    elif line == "54.244.207.95\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "35.93.99.15\n":
+    elif line == "52.88.3.155\n":
         pass
     elif line == "54.200.203.87\n":
         pass
