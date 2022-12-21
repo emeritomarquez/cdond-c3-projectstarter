80efb6d5afefa3d11b5b45139e71f43f9b2a5ab1
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-12-21 21:49:18 +0000

    rename ansible deploy roles tasks to main.yml

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "34.219.133.135\n":
+    elif line == "54.185.251.83\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "34.222.104.74\n":
+    elif line == "54.187.76.192\n":
         pass
 #Cloud9 Public
     elif line == "54.200.203.86\n":
