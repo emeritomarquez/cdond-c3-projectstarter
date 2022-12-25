e2057a013b125ea3fecb9a8891a1cba9c2ab3d32
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-12-25 19:32:42 +0000

    troubleshooting directory of ec2 backend instance

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "54.185.251.83\n":
+    elif line == "18.236.78.192\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "54.187.76.192\n":
+    elif line == "34.219.106.17\n":
         pass
     else:
         rows.append(line)
