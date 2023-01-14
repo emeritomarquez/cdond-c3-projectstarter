dc12805d33fd2b60986ed78ab9a13f8f654e08b9
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2023-01-14 17:11:25 +0000

    Corrections in npm run migrations

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "54.184.88.50\n":
+    elif line == "34.219.107.231\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "52.24.115.169\n":
+    elif line == "34.208.236.63\n":
         pass
     else:
         rows.append(line)
