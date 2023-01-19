5bc830b4400b8ef1ccce5501c43404f5d36074c1
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2023-01-19 19:41:39 +0000

    Downgrade node-migration image to 13.8.0 and reinstate restore_cache

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "54.185.251.83\n":
+    elif line == "34.219.107.231\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "54.187.76.192\n":
+    elif line == "34.208.236.63\n":
         pass
 #Cloud9 Public
     elif line == "54.200.203.86\n":
