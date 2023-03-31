d991ae0f91f5ea743e01a41f638525b35882ab30
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2023-03-31 14:39:00 +0000

    Add cloud front job

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "54.202.49.190\n":
+    elif line == "34.221.226.122\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "54.201.240.239\n":
+    elif line == "35.167.212.86\n":
         pass
 #typeorm2
     elif line == "54.149.54.106\n":
